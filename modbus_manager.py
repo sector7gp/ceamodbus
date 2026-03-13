bb009c1464aa98c7badf7e1e4f11faa49e304fda
Pablo Gonzalez <sector7gp@Pablos-MacBook-Air.local>
2026-03-12 22:26:49 -0300

    Corrected direction read and added missing polar, RS485, and version registers

--- modbus_manager.py
+++ modbus_manager.py
@@ -52,40 +52,51 @@ class ModbusManager:
         }
 
         # Block 1: Velocities (0x56 to 0x5F)
-        regs = self._read_safe(0x0056, 10)
-        if regs:
-            status["set_speed"] = regs[0]
-            status["feedback_speed"] = regs[9]
+        # Note: 0x5F is relative index 9 from 0x56
+        regs1 = self._read_safe(0x0056, 10)
+        if regs1:
+            status["set_speed"] = regs1[0]           # 0x0056
+            status["feedback_speed"] = regs1[9]      # 0x005F
             status["connected"] = True
 
         # Block 2: Logic states (0x66 to 0x6D)
-        regs = self._read_safe(0x0066, 8)
-        if regs:
-            status["is_enabled"] = regs[0] == 0   # 0=Enable, 1=Disable
-            status["is_braked"] = regs[4] == 0    # 0=Brake, 1=No Brake
-            status["is_forward"] = regs[7] == 1   # 1=Forward, 0=Backwards
+        # 0x66: Enable (idx 0)
+        # 0x6A: Brake (idx 4)
+        # 0x6D: Direction (idx 7)
+        regs2 = self._read_safe(0x0066, 8)
+        if regs2:
+            status["is_enabled"] = regs2[0] == 0    # 0=Enable, 1=Disable
+            status["is_braked"] = regs2[4] == 0     # 0=Brake, 1=No Brake
+            # Manual description: "1 it's spinning forward, if it's 0 it's spinning backwards"
+            status["is_forward"] = regs2[7] == 1    # 0x6D
 
         # Block 3: Alarm (0x76)
-        regs = self._read_safe(0x0076, 1)
-        if regs:
-            status["alarm_code"] = regs[0]
+        regs3 = self._read_safe(0x0076, 1)
+        if regs3:
+            status["alarm_code"] = regs3[0]
 
         # Block 4: Config (0x86 to 0x8A)
-        regs = self._read_safe(0x0086, 5)
-        if regs:
-            status["pole_pairs"] = regs[0]
-            status["acc_time"] = regs[4]
+        # 0x86: Polar logarithm (idx 0)
+        # 0x8A: Acc/Dec time (idx 4)
+        regs4 = self._read_safe(0x0086, 5)
+        if regs4:
+            status["pole_pairs"] = regs4[0]         # 0x0086
+            status["acc_time"] = regs4[4]           # 0x008A
 
-        # Block 5: Max speed (0x92)
-        regs = self._read_safe(0x0092, 1)
-        if regs:
-            status["max_analogue_speed"] = regs[0]
+        # Block 5: Max analogue speed (0x92)
+        regs5 = self._read_safe(0x0092, 1)
+        if regs5:
+            # Note: Max value range 0-60000
+            status["max_analogue_speed"] = regs5[0]
 
-        # Block 6: Connection & Version
-        regs = self._read_safe(0x00B6, 6)
-        if regs:
-            status["rs485_status"] = regs[0]
-            status["version"] = regs[5]
+        # Block 6: Connection & Version (0xB6, 0xBB)
+        # 0xB6: RS485 status
+        # 0xBB: Program version
+        # 0xBB - 0xB6 = 5. Read 6 registers.
+        regs6 = self._read_safe(0x00B6, 6)
+        if regs6:
+            status["rs485_status"] = regs6[0]       # 0xB6
+            status["version"] = regs6[5]            # 0xBB
 
         return status
 
