7e60ceb916767eb7bd590fb3439f105d96f12e6b
Pablo Gonzalez <sector7gp@Pablos-MacBook-Air.local>
2026-03-12 22:15:32 -0300

    Fix IndexError by increasing register count to 17

--- modbus_manager.py
+++ modbus_manager.py
@@ -28,16 +28,18 @@ class ModbusManager:
         """
         try:
             # Registers according to documentation
-            # 0x0056: Set speed, 0x005F: Feedback speed
-            # We can read a block or individual registers
-            res = self.client.read_holding_registers(address=0x0056, count=16, slave=self.slave_id)
+            # 0x0056: Set speed (index 0)
+            # 0x005F: Feedback speed (index 9, 0x5F-0x56=9)
+            # 0x0066: Enable state (index 16, 0x66-0x56=16) 
+            # We need to read at least 17 registers to reach 0x0066
+            res = self.client.read_holding_registers(address=0x0056, count=17, slave=self.slave_id)
             if res.isError():
                 return None
             
             data = {
                 "set_speed": res.registers[0],          # 0x0056
-                "feedback_speed": res.registers[9],     # 0x005F (0x5F - 0x56 = 9)
-                "is_enabled": res.registers[16],        # 0x0066 (0x66 - 0x56 = 16)
+                "feedback_speed": res.registers[9],     # 0x005F
+                "is_enabled": res.registers[16] == 0,    # 0x0066: 0=Enabled, 1=Disabled
             }
             return data
         except ModbusException as e:
